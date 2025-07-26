5cc9fd61d4509c6d3e39977c9dec01bfb7ee7bd9
katafuchix <katafuchix@gmail.com>
2025-07-26 11:48:48 +0900

    Initial Commit

---
+++ SwiftUI_ResizableHeaderExample/SwiftUI_ResizableHeaderExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ResizableHeaderExampleApp.swift
+//  SwiftUI_ResizableHeaderExample
+//
+//  Created by cano on 2025/07/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ResizableHeaderExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
